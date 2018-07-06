ec92a688433bdc6c1dc908f4bd15ad650e26bf81
mattpolski <mattpolski@gmail.com>
2018-07-06 19:00:25 +0200

    19.1

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -51,7 +51,7 @@ public class TrelloClient {
 
         public CreatedTrelloCard createNewCard (TrelloCardDto trelloCardDto) {
 
-            URI url = UriComponentsBuilder.fromHttpUrl(trelloConfig.getTrelloApiEndpoint() + "/members/mateuszpolski/cards")
+            URI url = UriComponentsBuilder.fromHttpUrl(trelloConfig.getTrelloApiEndpoint() + "/cards")
                     .queryParam("key", trelloConfig.getTrelloAppKey())
                     .queryParam("token", trelloConfig.getTrelloToken())
                     .queryParam("name", trelloCardDto.getName())
